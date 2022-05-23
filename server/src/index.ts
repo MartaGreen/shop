fcf8824f124909bc1edbd510b3b95b3b7aa055be
MartaGreen <iguana13131@gmail.com>
2022-05-23 19:53:43 +0400

    feat: add express router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import main from "./database/connection";
 
+// routes
+import userRoter from "./routes/user";
+
 const app = express();
 const port = Number(process.env.PORT) | 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+app.use("/user", userRoter);
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
