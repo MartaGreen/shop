874da475ec2ab33341fb7abde089713086634946
MartaGreen <iguana13131@gmail.com>
2022-05-23 19:42:40 +0400

    fix: add env file

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,15 +1,15 @@
+import "dotenv/config";
 import { createConnection } from "typeorm";
 
 const main = async () => {
   try {
     await createConnection({
       type: "postgres",
-      host: "ec2-34-246-227-219.eu-west-1.compute.amazonaws.com",
+      host: process.env.DB_HOST,
       port: 5432,
-      username: "bbhnodatlmhvrf",
-      password:
-        "015053b79ce6a380947483fb8a77063caed7cf4cfc26e24af0db2361ccb8f192",
-      database: "de78qnt8qokn54",
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       extra: {
         ssl: { rejectUnauthorized: false },
       },
